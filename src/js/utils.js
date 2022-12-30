8ff908adaeaecb8396a456b37e183ca97db17582
Pruthivi Thejan <pruthivithejan.code@gmail.com>
2022-12-30 20:01:36 +0530

    v.0.3.0

---
+++ src/js/utils.js
@@ -0,0 +1,17 @@
+
+export function slugify(text) {
+    return text
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/--+/g, '-')
+      .replace(/^-+/, '')
+      .replace(/-+$/, '');
+  }
+  
+  export function formatDate(date) {
+    return new Date(date).toLocaleDateString('en-US', {
+      timeZone: "UTC",
+    })
+  }
