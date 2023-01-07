fb9607a2f1c6fd7992af596abaf4ccc597ed2506
Pruthivi Thejan <pruthivithejan.code@gmail.com>
2023-01-07 13:14:59 +0530

    v0.4.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
+  site:'https://pruthivithejan.me',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
