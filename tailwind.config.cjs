aeedcb7066729394aae679f7e901e8bcaaf1796c
pruthivithejan <pruthivithejan.code@gmail.com>
2023-05-01 13:32:15 +0530

    Turning to Tailwind - 0.2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        trap: ["Trap"],
-        stretch: ["Stretch Pro"],
+        body: ["Trap"],
+        heading: ["Stretch Pro"],
       },
     },
   },
