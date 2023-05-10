11063c09ce09a0573a5bced6cdd18ed3fa308109
pruthivithejan <pruthivithejan.code@gmail.com>
2023-05-10 16:56:17 +0530

    Turning to Tailwind 0.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,23 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://pruthivithejan.me",
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-  ],
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  })]
 });
