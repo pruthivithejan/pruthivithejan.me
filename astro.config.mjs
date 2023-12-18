31f834383dde38c0b8f883e6acffbbd0b6cfcb48
pruthivithejan <pruthivithejan.code@gmail.com>
2023-12-18 15:24:16 +0530

    Removed Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,21 @@ import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://pruthivithejan.me",
-  integrations: [sitemap(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  })],
-  output: "server",
-  adapter: vercel()
+  integrations: [
+    sitemap(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
