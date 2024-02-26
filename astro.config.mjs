55f15d9747a10b1bf7c321e852e3bb2b1e3848cf
pruthivithejan <pruthivithejan.code@gmail.com>
2024-02-26 17:14:40 +0530

    Added blog components to a folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +22,6 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
+    svelte(),
   ],
 });
