4c7386107a255210518fd55d832d04adc073e9b0
pruthivithejan <pruthivithejan.code@gmail.com>
2024-02-27 22:03:10 +0530

    Added @astro/svelte

---
+++ src/components/Hero.svelte
@@ -0,0 +1,41 @@
+<script>
+  import RollingText from "@/components/animated/RollingText.svelte";
+
+  let words = ["Designer", "Developer"];
+</script>
+
+<div class="flex justify-center items-center mb-10">
+  <div
+    class=" grayscale flex justify-center items-center w-64 h-64 p-2 ml-4 origin-center transform scale-100 bg-clip-content overflow-hidden rounded-lg shadow-sm shadow-neutral-50"
+  >
+    <img
+      src={`https://avatars.githubusercontent.com/pruthivithejan`}
+      alt="Avatar"
+      class="rounded-md"
+      width="280"
+      height="280"
+    />
+  </div>
+
+  <!-- <div class="center">
+    <div class="thirteen">
+      <img
+        src={`https://avatars.githubusercontent.com/pruthivithejan`}
+        alt="Avatar"
+        class="rounded-md"
+        width="280"
+        height="280"
+      />
+    </div>
+  </div> -->
+</div>
+<h1 class="text-6xl">Pruthvi Thejan</h1>
+<h1 class="text-6xl font-body"><RollingText {words} /></h1>
+<div class="container py-10">
+  <h2 class="text-4xl">21</h2>
+  <h2 class="text-4xl">Sri Lankan</h2>
+  <h2 class="text-4xl">Undergratuate at USJP</h2>
+</div>
+
+<style>
+</style>
---
+++ src/components/animated/RollingText.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import { slide } from "svelte/transition";
+
+  export let words = [];
+  let index = 0;
+  let roller;
+
+  onMount(() => {
+    roller = setInterval(() => {
+      if (index === words.length - 1) index = 0;
+      else index++;
+    }, 1250);
+  });
+
+  onDestroy(() => {
+    clearInterval(roller);
+  });
+</script>
+
+{#key index}
+  <h1 transition:slide>{words[index]}</h1>
+{/key}
