46d951eac700ca3108c12f4b8a50563f79afcde9
pruthivithejan <pruthivithejan.code@gmail.com>
2024-04-10 13:27:36 +0530

    Added Pinterest Verification Tag

--- src/pages/api/sendEmail.json.ts
+++ src/pages/api/sendEmail.json.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import { Resend } from "resend";
 
+export const prerender = false;
+
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const POST: APIRoute = async ({ request }) => {
