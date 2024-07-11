aabe07df05eaa2eaddaec18b1359c73185af4fd8
pruthivithejan <pruthivithejan.code@gmail.com>
2024-07-11 20:26:37 +0530

    Changed spark-button

--- src/components/stack-marquee.svelte
+++ src/components/stack-marquee.svelte
@@ -1,67 +1,67 @@
 <script>
-import { onMount } from "svelte";
-import * as Tooltip from "@/components/ui/tooltip";
-import SparkButton from "@/components/animated/spark-button.astro";
-const tools = [
-	{
-		name: "Notion",
-		icon: "/images/apps/app_notion.jpg",
-		link: "https://apps.apple.com/us/app/notion-notes-docs-tasks/id1232780281",
-	},
-	{
-		name: "Expedia",
-		icon: "/images/apps/app_expedia.jpg",
-		link: "https://apps.apple.com/us/app/expedia-hotels-flights-car/id427916203",
-	},
-	{
-		name: "X (Twitter)",
-		icon: "/images/apps/app_x.jpg",
-		link: "https://apps.apple.com/de/app/x/id333903271",
-	},
-	{
-		name: "Flighty",
-		icon: "/images/apps/app_flighty.jpg",
-		link: "https://apps.apple.com/us/app/flighty-live-flight-tracker/id1358823008",
-	},
-	{
-		name: "Spotify",
-		icon: "/images/apps/app_spotify.jpg",
-		link: "https://apps.apple.com/us/app/spotify-music-and-podcasts/id324684580",
-	},
-	{
-		name: "Finanzguru",
-		icon: "/images/apps/app_finanzguru.jpg",
-		link: "https://apps.apple.com/us/app/finanzguru-konten-vertr%C3%A4ge/id1214803607",
-	},
-	{
-		name: "Messages",
-		icon: "/images/apps/app_messages.jpg",
-		link: "https://apps.apple.com/us/app/messages/id1146560473",
-	},
-	{
-		name: "Cron",
-		icon: "/images/apps/app_cron.jpg",
-		link: "https://apps.apple.com/us/app/cron-calendar/id1607562761",
-	},
-];
+  import { onMount } from "svelte";
+  import * as Tooltip from "@/components/ui/tooltip";
+  import SparkButton from "@/components/animated/spark-button.astro";
+  const tools = [
+    {
+      name: "Notion",
+      icon: "/images/apps/app_notion.jpg",
+      link: "https://apps.apple.com/us/app/notion-notes-docs-tasks/id1232780281",
+    },
+    {
+      name: "Expedia",
+      icon: "/images/apps/app_expedia.jpg",
+      link: "https://apps.apple.com/us/app/expedia-hotels-flights-car/id427916203",
+    },
+    {
+      name: "X (Twitter)",
+      icon: "/images/apps/app_x.jpg",
+      link: "https://apps.apple.com/de/app/x/id333903271",
+    },
+    {
+      name: "Flighty",
+      icon: "/images/apps/app_flighty.jpg",
+      link: "https://apps.apple.com/us/app/flighty-live-flight-tracker/id1358823008",
+    },
+    {
+      name: "Spotify",
+      icon: "/images/apps/app_spotify.jpg",
+      link: "https://apps.apple.com/us/app/spotify-music-and-podcasts/id324684580",
+    },
+    {
+      name: "Finanzguru",
+      icon: "/images/apps/app_finanzguru.jpg",
+      link: "https://apps.apple.com/us/app/finanzguru-konten-vertr%C3%A4ge/id1214803607",
+    },
+    {
+      name: "Messages",
+      icon: "/images/apps/app_messages.jpg",
+      link: "https://apps.apple.com/us/app/messages/id1146560473",
+    },
+    {
+      name: "Cron",
+      icon: "/images/apps/app_cron.jpg",
+      link: "https://apps.apple.com/us/app/cron-calendar/id1607562761",
+    },
+  ];
 
-onMount(() => {
-	iconsContainer.addEventListener("scroll", checkScroll);
-});
+  onMount(() => {
+    iconsContainer.addEventListener("scroll", checkScroll);
+  });
 
-function openLink(tool) {
-	if (typeof window !== "undefined" && window.innerWidth > 768) {
-		window.open(tool.link);
-	}
-}
+  function openLink(tool) {
+    if (typeof window !== "undefined" && window.innerWidth > 768) {
+      window.open(tool.link);
+    }
+  }
 </script>
 
 <section
   class="w-full flex flex-col items-center lg:max-w-none max-w-md mx-auto"
 >
-  <a href="/stack">
+  <!-- <a href="/stack">
     <SparkButton text="View Stack" />
-  </a>
+  </a> -->
   <div class="lg:w-5/12 w-full">
     <div class="flex items-center relative">
       <div
--- src/content/blog/How to create a newsletter with Resend.md
+++ src/content/blog/How to create a newsletter with Resend.md
@@ -4,7 +4,7 @@ date: 2024-07-03
 edited:
 image: { src: "/images/blog/Resend.png", alt: "A picture of a coder" }
 description: CSS is a great way to style your website. In fact, there are no alternatives! Learn how to use it here.
-draft: false
+draft: true
 category: Guides
 ---
 
