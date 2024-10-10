d8df85e5b2dda576474a658a51a5523b286fd3c2
pruthivithejan <pruthivithejan.code@gmail.com>
2024-10-10 10:18:58 +0530

    Removed @astrojs/ partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,32 +4,26 @@ import path from "path";
 import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://pruthivithejan.me",
-	output: "hybrid",
-	adapter: cloudflare(),
-	resolve: {
-		alias: {
-			$lib: path.resolve("./src/lib"),
-		},
-	},
-	integrations: [
-		sitemap(),
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		partytown({
-			config: {
-				forward: ["dataLayer.push"],
-			},
-		}),
-		svelte(),
-		icon(),
-	],
+  site: "https://pruthivithejan.me",
+  output: "hybrid",
+  adapter: cloudflare(),
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
+  integrations: [
+    sitemap(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    svelte(),
+    icon(),
+  ],
 });
